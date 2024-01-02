0947fa4fe0d4e3935fa20e022af23eca292421d9
Isabel Gonçalves <igl@cesar.school>
2024-01-02 18:42:29 -0300

    mudanças em formulário javascript

--- js/formulario.js
+++ js/formulario.js
@@ -2,9 +2,9 @@ const formulario = document.querySelector("form");
 
 function formularioEnviado(resposta){
     if (resposta.ok){
-        formulario.innerHTML = "<p class='font-2-1' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'> <span style= 'color: #317a00'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em até 24 horas."
+        formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'> <span style= 'color: #317a00'>Mensagem enviada</span>, em breve entraremos em contato. Geralmente respondemos em até 24 horas."
     }else{
-        formulario.innerHTML = "<p class='font-2-1' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'> <span style= 'color: #E00000';>Erro no envio</span>, formulário indisponível. Entre em contato diretamente pelo <a href='www.linkedin.com/in/isabel-gonçalves/' style='text-decoration: underline';>linkedIn</a>. </p>"
+        formulario.innerHTML = "<p class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'> <span style= 'color: #E00000';>Erro no envio</span>, formulário indisponível. Entre em contato diretamente pelo <a href='https://www.linkedin.com/in/isabel-gon%C3%A7alves/' style='text-decoration: underline';>linkedIn</a>. </p>"
     }
 }
 
